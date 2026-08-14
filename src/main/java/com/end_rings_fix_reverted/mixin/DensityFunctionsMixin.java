7ef8d313cdb99a32bb68b3d7b825dd8729cc4b0d
X-M-AwA <minecraft.xiaog@gmail.com>
2026-08-14 16:11:50 +0800

    同步快照8

--- src/main/java/com/end_rings_fix_reverted/mixin/DensityFunctionsMixin.java
+++ src/main/java/com/end_rings_fix_reverted/mixin/DensityFunctionsMixin.java
@@ -2,18 +2,18 @@ package com.end_rings_fix_reverted.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.util.Mth;
-import net.minecraft.world.level.levelgen.DensityFunctions;
+import net.minecraft.world.level.levelgen.densityfunction.generator.EndIslandFunction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(DensityFunctions.EndIslandDensityFunction.class)
+@Mixin(EndIslandFunction.class)
 public abstract class DensityFunctionsMixin {
     @ModifyConstant(
             method = "getHeightValue",
             constant = @Constant(floatValue = -100.0F)
     )
-    private static float seyValue(float constant, @Local(ordinal = 0, argsOnly = true) int subSectionX, @Local(ordinal = 1, argsOnly = true) int subSectionZ) {
+    private static float setValue(float constant, @Local(ordinal = 0, argsOnly = true) int subSectionX, @Local(ordinal = 1, argsOnly = true) int subSectionZ) {
         float i = 100.0F - Mth.sqrt(subSectionX * subSectionX + subSectionZ * subSectionZ) * 8.0F;
         return Math.clamp(i, -100.0F, 80.0F);
     }
